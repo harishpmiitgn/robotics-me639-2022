e95bd185a1e756d2a4897162d4f376f64d6a07d7
Souritra Garai <46619335+Souritra-Garai@users.noreply.github.com>
2022-09-17 12:55:58 +0530

    Rectified mistake

--- assignment-3/code/Prob-11-Dynamics.py
+++ assignment-3/code/Prob-11-Dynamics.py
@@ -1,14 +1,11 @@
 import sympy
 
-t, g = sympy.symbols('t, g')
+g = sympy.symbols('g')
 
-def getEquationsOfMotion(D, V, q, tau) :
+def getEquationsOfMotion(D, V, q, q_dot, q_ddot, tau) :
 
 	n = len(q)
 	equations = []
-
-	q_dot  = [sympy.Derivative(q[i], t) for i in range(n)]
-	q_ddot = [sympy.Derivative(q_dot[i], t) for i in range(n)]
 
 	for k in range(n) :
 
@@ -22,18 +19,22 @@ def getEquationsOfMotion(D, V, q, tau) :
 
 			for j in range(n) :
 
-				expr += 0.5 * (sympy.Derivative(D[k][j], q[i]) + sympy.Derivative(D[k][i], q[j]) - sympy.Derivative(D[i][j], q[k]))
+				expr += 0.5 * (sympy.Derivative(D[k][j], q[i]) + sympy.Derivative(D[k][i], q[j]) - sympy.Derivative(D[i][j], q[k])) * q_dot[i] * q_dot[j]
 
 		expr += sympy.Derivative(V, q[k])
 
-		equations.append(expr)
+		equations.append(sympy.simplify(expr))
 
 	return equations
 
 if __name__ == '__main__' :
 
 	q1, q2 = sympy.symbols('q_1, q_2')
+	q1_dot, q2_dot = sympy.symbols('q\dot_1, q\dot_2')
+	q1_ddot, q2_ddot = sympy.symbols('q\ddot_1, q\ddot_2')
 	q = [q1, q2]
+	q_dot = [q1_dot, q2_dot]
+	q_ddot = [q1_ddot, q2_ddot]
 
 	m1, m2 = sympy.symbols('m_1, m_2')
 	I1, I2 = sympy.symbols('I_1, I_2')
@@ -48,7 +49,7 @@ if __name__ == '__main__' :
 
 	V = m1 * g * (l1/2) * sympy.sin(q1) + m2 * g * ( l1 * sympy.sin(q1) + (l2/2) * sympy.sin(q2))
 
-	eqns = getEquationsOfMotion(D, V, q, sympy.symbols('tau_1, tau_2'))
+	eqns = getEquationsOfMotion(D, V, q, q_dot, q_ddot, sympy.symbols('tau_1, tau_2'))
 
 	print(eqns)
 
