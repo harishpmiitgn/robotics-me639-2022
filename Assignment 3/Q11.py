8a78853374f23d4f3fe550fc22a0ce4855564449
Sanskar201 <64640289+Sanskar201@users.noreply.github.com>
2022-09-18 02:17:26 +0530

    Add files via upload

--- Assignment 3/Q11.py
+++ Assignment 3/Q11.py
@@ -5,8 +5,8 @@ for i in range(n):
     globals()['q%s' % str(i+1)] = sympy.Symbol('q'+str(i+1))
     globals()['q_dot%s' % str(i+1)] = sympy.Symbol('q_dot'+str(i+1))
     globals()['q_dot_dot%s' % str(i+1)] = sympy.Symbol('q_dot_dot'+str(i+1))
-m1,m2,l1,l2,g=[1,1,1,1,9.8]
-D=numpy.array([[m1*l2**2/3+m2*l2**2, m2*l1*l2/2*sympy.cos(q2-q1)],[m2*l1*l2/2*sympy.cos(q2-q1), m2*l2**2/3]])
+m1,m2,l1,l2,g=[sympy.Symbol('m1'),sympy.Symbol('m2'),sympy.Symbol('l1'),sympy.Symbol('l2'),sympy.Symbol('g')]
+D=numpy.array([[m1*l1**2/3+m2*l1**2, m2*l1*l2*sympy.cos(q2-q1)/2],[m2*l1*l2*sympy.cos(q2-q1)/2, m2*l2**2/3]])
 V=m1*g*l1/2*sympy.sin(q1)+m2*g*(l1*sympy.sin(q1)+l2/2*sympy.sin(q2))
 phi=numpy.array([[sympy.diff(V, q1)],[sympy.diff(V, q2)]])
 q=numpy.array([[]])
@@ -25,7 +25,7 @@ C=[0]*n
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            C[k]+=0.5*(sympy.diff(D[k][j], "q"+str(i+1))+sympy.diff(D[k][i], "q"+str(j+1))+sympy.diff(D[i][j], "q"+str(k+1)))*sympy.Symbol("q"+str(i+1))*sympy.Symbol("q"+str(j+1))
+            C[k]+=0.5*(sympy.diff(D[k][j], "q"+str(i+1))+sympy.diff(D[k][i], "q"+str(j+1))-sympy.diff(D[i][j], "q"+str(k+1)))*sympy.Symbol("q_dot"+str(i+1))*sympy.Symbol("q_dot"+str(j+1))
 EL=D@q_dot_dot+phi+numpy.transpose([C])
 for i in range(len(EL)):
     print(EL[i][0],end="")
