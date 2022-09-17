587987ecc8769808295a9a1229218f03ffab75d3
Sanskar201 <64640289+Sanskar201@users.noreply.github.com>
2022-09-17 18:19:03 +0530

    Add files via upload

---
+++ Assignment 3/Q11.py
@@ -0,0 +1,32 @@
+import numpy
+import sympy
+n=2
+for i in range(n):
+    globals()['q%s' % str(i+1)] = sympy.Symbol('q'+str(i+1))
+    globals()['q_dot%s' % str(i+1)] = sympy.Symbol('q_dot'+str(i+1))
+    globals()['q_dot_dot%s' % str(i+1)] = sympy.Symbol('q_dot_dot'+str(i+1))
+m1,m2,l1,l2,g=[1,1,1,1,9.8]
+D=numpy.array([[m1*l2**2/3+m2*l2**2, m2*l1*l2/2*sympy.cos(q2-q1)],[m2*l1*l2/2*sympy.cos(q2-q1), m2*l2**2/3]])
+V=m1*g*l1/2*sympy.sin(q1)+m2*g*(l1*sympy.sin(q1)+l2/2*sympy.sin(q2))
+phi=numpy.array([[sympy.diff(V, q1)],[sympy.diff(V, q2)]])
+q=numpy.array([[]])
+for i in range(n):
+    q=numpy.append(q,[[globals()['q%s' % str(i+1)]]],axis=1)
+q=numpy.transpose(q)
+q_dot=numpy.array([[]])
+for i in range(n):
+    q_dot=numpy.append(q_dot,[[globals()['q_dot%s' % str(i+1)]]],axis=1)
+q_dot=numpy.transpose(q_dot)
+q_dot_dot=numpy.array([[]])
+for i in range(n):
+    q_dot_dot=numpy.append(q_dot_dot,[[globals()['q_dot_dot%s' % str(i+1)]]],axis=1)
+q_dot_dot=numpy.transpose(q_dot_dot)
+C=[0]*n
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            C[k]+=0.5*(sympy.diff(D[k][j], "q"+str(i))+sympy.diff(D[k][i], "q"+str(j))+sympy.diff(D[i][j], "q"+str(k)))*sympy.Symbol("q"+str(i))*sympy.Symbol("q"+str(j))
+EL=D@q_dot_dot+phi+numpy.transpose([C])
+for i in range(len(EL)):
+    print(EL[i][0],end="")
+    print(" = tau_"+str(i+1))
